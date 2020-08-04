ad5f2e1b8d15737788e5ab4600eecba4fe354505
Matheus B <matheus_ob59@hotmail.com>
2020-08-04 18:13:21 -0300

    Next Level Week Day 1

---
+++ web-app/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem(){
+    return(
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars1.githubusercontent.com/u/31048764?s=460&u=4993d33703a05eecdc4ff13be7dc13b6bfe0e88f&v=4" alt="Matheus Oliveira"/>
+                <div>
+                    <strong>Matheus Oliveira</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br/><br/>
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
